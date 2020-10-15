5e8a7287750105be40d6a3680cb9617c066f28d6
Marek Lahk <Marek123610@gmail.com>
2020-10-15 21:20:06 +0300

    Fix Cors errors?

--- src/main/java/com/ibay/backend/config/AppConf.java
+++ src/main/java/com/ibay/backend/config/AppConf.java
@@ -1,18 +1,27 @@
 package com.ibay.backend.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConf implements WebMvcConfigurer {
+public class AppConf  {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowedOrigins("*");
+
+    @Bean
+    public WebMvcConfigurer configurer() {
+        return new WebMvcConfigurer() {
+
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*");
+            }
+        };
     }
 }
