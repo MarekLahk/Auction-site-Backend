02516e429c244599c3a645f87afc73ff3d53ae5e
Marek Lahk <malahk@ttu.ee>
2020-10-24 23:29:18 +0300

    BugFixes, question answers

---
+++ src/main/java/com/ibay/backend/theory/question3/Abstraction.java
@@ -0,0 +1,18 @@
+package com.ibay.backend.theory.question3;
+
+public class Abstraction {
+
+    //todo
+    // Abstra-ca-dabra
+
+    //todo p1
+    // In your words (do not use wiki definitions)
+    // What is abstraction? (P.S It has nothing to do with keyword abstract)
+    // Answer: Abstraction is hiding the innerworkings of something from the user.
+	//			OR making a standard that all children must follow. Same methods and variables etc.
+
+    //todo p2
+    // Create an abstraction of an animal of your choice.
+    // It should have at least 1 property and 1 method.
+    // Create it as a real java class inside this package.
+}
---
+++ src/main/java/com/ibay/backend/theory/question4/DependencyInjection.java
@@ -0,0 +1,35 @@
+package com.ibay.backend.theory.question4;
+
+public class DependencyInjection {
+
+    //todo
+    // One of the reasons we use Spring is that it gives us good support for
+    // Dependency Injection (DI) and Inversion of Control (IoC)
+
+    //todo p1
+    // In your words (do not use wiki definitions)
+    // What is Dependency Injection?
+    // Answer: Dependency injection is a way of automatically creating objects that
+	//			are then passed to all the classes that need it.
+
+    //todo p2
+    // Bring example from your code of Dependency Injection.
+    // Paste your code here, but comment it out
+	
+	//    @Autowired
+    //public AuctionService(AuctionDao auctionDao, UserDao userDao, IdGenerator idGenerator) {
+	//
+    //    this.auctionDao = auctionDao;
+    //    this.userDao = userDao;
+    //    this.idGenerator = idGenerator;
+    //}
+
+    //todo p3
+    // Name 2 benefits of Dependency Injection
+    // 1 No multiple instances of classes are created
+    // 2 Reduces boilerplate code
+
+    //todo p4
+    // Name 1 disadvantage of Dependency Injection
+    // 1 Can be difficult to understand which dependecy is used in which situation
+}
