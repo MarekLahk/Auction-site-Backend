72bbb05e3d7886973401ff38714aeb182ad224d3
Stefanie <stkort@ttu.ee>
2020-10-25 10:34:22 +0200

    Stefanie Kortšak question 6 chocolate

---
+++ src/main/java/com/ibay/backend/theory/question6/chocolate/Cake.java
@@ -0,0 +1,30 @@
+package com.ibay.backend.theory.question6.chocolate;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class Cake {
+
+    private Long id;
+    private String size;
+    private String sweetness;
+    private List<String> ingredients;
+    private List<String> toppings;
+
+    public Cake(@JsonProperty Long id,
+                @JsonProperty String size,
+                @JsonProperty String sweetness,
+                @JsonProperty List<String> ingredients,
+                @JsonProperty List<String > toppings) {
+        this.id = id;
+        this.size = size;
+        this.sweetness = sweetness;
+        this.ingredients = ingredients;
+        this.toppings = toppings;
+    }
+
+    public Cake(){};
+}
